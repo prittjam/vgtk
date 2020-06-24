c4d9d44c65ba1a10e0b796ec72fd6a922a663ec1
ylochman <lochman.y@gmail.com>
2020-06-24 17:29:24 -0400

    .

--- scene_sim/+MEAS/make_minimal_sample_arc.m
+++ scene_sim/+MEAS/make_minimal_sample_arc.m
@@ -1,7 +1,7 @@
-function [L, arcs, circles] = make_minimal_sample_arc(n, wplane, hplane, cam, direction, varargin)
+function [L, arcs, circles] = make_minimal_sample_arc(N, wplane, hplane, cam, direction, varargin)
     %
     % Args:
-    %   n
+    %   N
     %   wplane
     %   hplane
     %   cam
@@ -15,20 +15,75 @@ function [L, arcs, circles] = make_minimal_sample_arc(n, wplane, hplane, cam, di
     cfg.num_pts = 110;
     cfg = cmp_argparse(cfg, varargin{:});
 
-    assert(n > 1)
+    assert(N > 1)
     if nargin < 4 || isempty(direction)
         direction = 'rand';
     end
-    [L, S] = CSPOND_SET.lines(n, wplane, hplane,...
+    [L, S] = CSPOND_SET.lines(N, wplane, hplane,...
                              'direction', direction, 'regular', true);
-    S = reshape(S,6,[]);
+    l = cam.P' \ L;
+    s = RP2.mtimesx(cam.P, reshape(S,6,[]));
+    %%%%%%%%%%%%%%%% WORKAROUND FOR WILDENAUER'S SOLVER
+    if N==5
+        l = l(:,1:3);
+        s = s(:,1:3);
+        [L1, S1] = CSPOND_SET.lines(2, wplane, hplane,...
+                             'direction', [0 1], 'regular', true);
+        l1 = cam.P' \ L1;
+        s1 = RP2.mtimesx(cam.P, reshape(S1,6,[]));
+        l = [l l1(:,1)];
+        s = [s s1(:,1)];
+
+        u = PT.renormI(cam.P * eye(3));
+        L = [u(2,3); -u(1,3); 0];
+
+        % Segments
+        N = 1;
+        v = [-wplane/2 wplane/2 -hplane/2 hplane/2];
+        top = repmat(cross([v(1) v(3) 1]', [v(2) v(3) 1]'), 1, N);
+        bottom = repmat(cross([v(1) v(4) 1]', [v(2) v(4) 1]'), 1, N);
+        left = repmat(cross([v(1) v(3) 1]', [v(1) v(4) 1]'), 1, N);
+        right = repmat(cross([v(2) v(3) 1]', [v(2) v(4) 1]'), 1, N);
+        pts = [LINE.intersect(L, top);...
+            LINE.intersect(L, bottom);...
+            LINE.intersect(L, left);...
+            LINE.intersect(L, right)];
+        ia = [repmat((pts(1,:) > v(1)) & (pts(1,:) < v(2)), 2, 1);...
+            repmat((pts(3,:) > v(1)) & (pts(3,:) < v(2)), 2, 1);...
+            repmat((pts(6,:) > v(3)) & (pts(6,:) < v(4)), 2, 1);...
+            repmat((pts(8,:) > v(3)) & (pts(8,:) < v(4)), 2, 1)];
+        edges = reshape(pts(ia(:)),2,[]);
+        edges = reshape(edges, 4, N);
+        k0 = rand(1, N) * 0.3;
+        k1 = rand(1, N) * 0.3 + 0.7;
+        s0 = edges(1:2,:) + k0 .* (edges(3:4,:) - edges(1:2,:));
+        s1 = edges(1:2,:) + k1 .* (edges(3:4,:) - edges(1:2,:));
+        S = [PT.homogenize(s0); PT.homogenize(s1)];
+        s2 = reshape(S, 6, []);
+        l2 = L;
+
+        l = [l l2];
+        s = [s s2];
+
+        assert(all(abs(dot(l(:,1:3),u(:,[1 1 1])))<1e-9))
+        assert(all(abs(dot(l(:,4),u(:,2)))<1e-9))
+        assert(all(abs(dot(l(:,5),u(:,3)))<1e-9))
+    end
+    %%%%%%%%%%%%%%%%
+
     project_fn = str2func(['LINE.' cam.proj_fn]);
-    circles = project_fn(cam.P' \ L, cam.K, cam.proj_params);
+    circles = project_fn(l, cam.K, cam.proj_params);
     project_fn = str2func(['RP2.' cam.proj_fn]);
-    s = project_fn(RP2.mtimesx(cam.P, S), cam.K, cam.proj_params);
-    % s = CIRCLE.project(reshape([circles; circles], 3, []), s);
+    s = project_fn(s, cam.K, cam.proj_params);
     [p, n] = ARC.get_midpoint(circles, s);
     circles = [circles; p; n];
-
     arcs = ARC.sample(circles, s, 'num_pts', cfg.num_pts);
+
+    % %%%%%%%%%%%%%%%% DEBUG
+    % close all
+    % CIRCLE.draw(circles,'Color',[1 1 1 2 3])
+    % ARC.draw(arcs,'LineWidth',2,'Color',[1 1 1 2 3])
+    % axis equal
+    % keyboard
+    % %%%%%%%%%%%%%%%%
 end

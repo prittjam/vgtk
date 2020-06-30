fe479b89ae3be8ed02ef7cdd9db65cbf7750f306
ylochman <lochman.y@gmail.com>
2020-06-30 00:04:06 -0400

    .

--- +CAM/make_lens.m
+++ +CAM/make_lens.m
@@ -1,12 +1,4 @@
-function cam = make_lens(cam, proj_params, A, proj_model);
-    if nargin < 4 || isempty(proj_model) || strcmp(proj_model, 'div')
-        proj_model = 'div';
-        cam.q_norm = proj_params;
-        cam.A = A;
-        unnorm_fun = str2func(['CAM.unnormalize_' proj_model]);
-        cam.q = unnorm_fun(cam.q_norm, cam.A);
-    end
-
+function cam = make_lens(cam, proj_params, proj_model);
     cam.backproj_fn = ['backproject_' proj_model];
     cam.proj_fn = ['project_' proj_model];
     cam.proj_model = proj_model;

46ada2680b0b58620b7fd341161a6865c32223d3
ylochman <lochman.y@gmail.com>
2020-07-20 16:07:00 -0400

    .

--- scene_sim/+SCENE/manhattan_arcs.m
+++ scene_sim/+SCENE/manhattan_arcs.m
@@ -10,7 +10,11 @@ function gt = manhattan_arcs()
     cam = CAM.make_lens(cam, q, 'div');
     cam = CAM.make_viewpoint(cam);
 
-    cam.vp = cam.P34 * [e; zeros(1,3)];
+    cam.vp_ud = cam.P34 * [e; zeros(1,3)];
+
+    cc = [570; 520];
+    T = cam.K;
+    T(1:2,3) = cc;
 
     wplane = 10;
     hplane = 10;
@@ -34,63 +38,48 @@ function gt = manhattan_arcs()
     end
 
     project_fn = str2func(['LINE.' cam.proj_fn]);
-    circles = project_fn(lines, cam.K, cam.proj_params);
+    circles = project_fn(lines, T, cam.proj_params);
     project_fn = str2func(['RP2.' cam.proj_fn]);
-    s = project_fn(endpoints, cam.K, cam.proj_params);
+    s = project_fn(endpoints, T, cam.proj_params);
     [p, n] = ARC.get_midpoint(circles, s);
     arcs = ARC.sample(circles, s, 'num_pts', 100);
 
     vp_labels = [ones(1,N(1)) 2*ones(1,N(2)) 3*ones(1,N(3))];
 
-    vp = project_fn(cam.vp, cam.K, cam.proj_params);
+    vp = project_fn(cam.vp_ud, T, cam.proj_params);
 
-    circles_norm = CIRCLE.normalize(circles, cam.K);
-    arcs_norm = ARC.normalize(arcs, cam.K);
-    vp_norm = RP2.normalize(vp, cam.K);
+    circles_norm = CIRCLE.normalize(circles, T);
+    arcs_norm = ARC.normalize(arcs, T);
+    vp_norm = RP2.normalize(vp, T);
     
-    % %%%%%%%%%%%%%%%% DRAW UNDISTORTED
-    % close all
-    % LINE.draw(gca,lines,'Color','b')
-    % ARC.draw(arcs,'LineWidth',2,'Color',vp_labels,'MarkerSize',10)
-    % GRID.draw(s,'Color',vp_labels,'Size',10)
-    % GRID.draw(cam.vp,'Size',30)
-    % axis equal
-    % keyboard
-    % %%%%%%%%%%%%%%%%
-    % % %%%%%%%%%%%%%%%% DRAW DISTORTED
-    % close all
-    % CIRCLE.draw(gt.circles,'Color',gt.vp_labels)
-    % ARC.draw(num2cell(reshape(gt.arcs,[100,3,5])),'LineWidth',2,'Color',gt.vp_labels,'MarkerSize',10)
-    % GRID.draw(gt.vp,'Size',30)
-    % axis equal
-    % keyboard
-    % % %%%%%%%%%%%%%%%%
-    % %%%%%%%%%%%%%%%% DRAW NORM
-    % close all
-    % figure
-    % CIRCLE.draw(circles_norm,'Color',vp_labels)
-    % ARC.draw(arcs_norm,'LineWidth',2,'Color',vp_labels,'MarkerSize',10)
-    % GRID.draw(vp_norm,'Size',30)
-    % axis equal
-    % keyboard
-    % %%%%%%%%%%%%%%%%
 
     gt = struct();
     gt.K = cam.K;
     gt.R = cam.R;
     gt.t = cam.c;
-    gt.vp_ud = cam.vp;
+    gt.cc = cc;
+    gt.vp_ud = cam.vp_ud;
     gt.vp = vp;
-    gt.vp_norm = vp_norm;
+    % gt.vp_norm = vp_norm;
     gt.q = cam.proj_params;
     gt.imsize_x = cam.nx;
     gt.imsize_y = cam.ny;
     gt.circles = circles;
-    gt.circles_norm = circles_norm;
+    % gt.circles_norm = circles_norm;
     gt.arcs = arcs;
-    gt.arcs_norm = [arcs_norm{:}];
+    % gt.arcs_norm = [arcs_norm{:}];
     gt.vp_labels = vp_labels;
     % savejson2(gt, GetFullPath('~/gt.json'));
     % save('~/gt.mat', 'gt')
     % keyboard
+
+    % %%%%%%%%%%%%%%%% DRAW DISTORTED
+    % close all
+    % CIRCLE.draw(gt.circles,'Color',gt.vp_labels)
+    % ARC.draw(gt.arcs,'LineWidth',2,'Color',gt.vp_labels,'MarkerSize',10)
+    % GRID.draw(gt.vp,'Size',30)
+    % GRID.draw([gt.cc; 1],'Size',30,'Color','k')
+    % axis equal
+    % keyboard
+    % %%%%%%%%%%%%%%%%
 end

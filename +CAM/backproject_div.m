9fb2ad2d1421df25a99d486a9e5ab58ad071e1b6
ylochman <lochman.y@gmail.com>
2020-07-21 00:31:32 -0400

    .

--- +CAM/backproject_div.m
+++ +CAM/backproject_div.m
@@ -1,7 +1,12 @@
 function v = backproject_div(u, K, proj_params)
-    % proj_params -- q
+    % proj_params -- [q, c]
 
+    % Radial distortion
     q = proj_params(1);
+    
+    % Shift by distortion center
+    c = proj_params([2 3]);
+    
     if abs(q) > 0
         m = size(u,1);
         if (m == 2)
@@ -11,10 +16,16 @@ function v = backproject_div(u, K, proj_params)
         end
         if ~isempty(K)
             v = K \ PT.renormI(v);
+            c = K \ [c'; 1];
         end
+
+        C = [1 0 c(1); 0 1 c(2); 0 0 1];
+        v = C \ v;
 
         v(3,:) = 1+q*(v(1,:).^2+v(2,:).^2);
         
+        v = C * v;
+
         if ~isempty(K)
             v(1:2,:) = bsxfun(@rdivide,v(1:2,:),v(3,:)); 
             v(3,:) = 1;
--- +CAM/project_div.m
+++ +CAM/project_div.m
@@ -1,7 +1,12 @@
 function v = project_div(u, K, proj_params)
-    % proj_params -- q
+    % proj_params -- [q, c]
 
+    % Radial distortion
     q = proj_params(1);
+    
+    % Shift by distortion center
+    c = proj_params([2 3]);
+
     if abs(q) > 0
         m = size(u,1);
         if (m == 2)
@@ -11,7 +16,11 @@ function v = project_div(u, K, proj_params)
         end
         if ~isempty(K)
             v = K \ PT.renormI(v);
+            c = K \ [c'; 1];
         end
+
+        C = [1 0 c(1); 0 1 c(2); 0 0 1];
+        v = C \ v;
 
         R = vecnorm(v(1:2,:),2,1);
         Z = v(3,:);
@@ -20,6 +29,8 @@ function v = project_div(u, K, proj_params)
         v(1:2,:) = v(1:2,:) .* r ./ R;
         v(3,:) = 1;
         
+        v = C * v;
+
         if ~isempty(K)
             v = K * v;
         end

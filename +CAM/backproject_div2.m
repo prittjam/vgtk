461a5d6a63a54fdb29e7fe857def4ebd966e2e15
James Pritts <jbpritts@gmail.com>
2020-07-22 10:29:16 -0400

    changes

--- +CAM/backproject_div2.m
+++ +CAM/backproject_div2.m
@@ -1,45 +1,18 @@
-function v = backproject_div(u, K, proj_params, K_new)
-    % proj_params -- [q, c]
-
-    % Radial distortion
-    q = proj_params(1);
-    
-    % Shift by distortion center
-    if numel(proj_params) == 3
-        c = proj_params([2 3]);
-    else
-        if ~isempty(K)
-            c = [0; 0];
-        else
-            c = K(1:2,3);
-        end
+function x = backproject_div2(x,K,q,cc,Kp)
+    if q == 0
+        return
     end
     
-    if abs(q) > 0
-        m = size(u,1);
-        if (m == 2)
-            v = PT.homogenize(u);
-        else
-            v = u;
-        end
-        if ~isempty(K)
-            v = K \ PT.renormI(v);
-            c = K \ [c'; 1];
-        end
-
-        C = [1 0 c(1); 0 1 c(2); 0 0 1];
-        v = C \ v;
-
-        v(3,:) = 1+q*(v(1,:).^2+v(2,:).^2);
-        
-        v = C * v;
-
-        if nargin==4 && ~isempty(K_new)
-            v(1:2,:) = bsxfun(@rdivide,v(1:2,:),v(3,:)); 
-            v(3,:) = 1;
-            v = K_new * v;
-        end
-    else
-        v = u;
+    if isempty(cc) || nargin < 4
+        cc = [0 0]';
     end
-end
+    
+    if nargin < 5
+        Kp = eye(3);
+    end
+
+    C = eye(3);
+    C(1:2,3) = cc(1:2);
+    x = C\K\RP2.renormI(x);
+    x(3,:) = 1+q*(sum(x(1:2,:).^2));
+    x = Kp*C*RP2.renormI(x);
--- +CAM/project_div2.m
+++ +CAM/project_div2.m
@@ -1,53 +1,25 @@
-function v = project_div(u, K_new, proj_params, K)
-    % proj_params -- [q, c]
-
-    % Radial distortion
-    q = proj_params(1);
-    
-    % Shift by distortion center
-    if numel(proj_params) == 3
-        c = proj_params([2 3]);
-    else
-        if nargin==4 && ~isempty(K)
-            c = K(1:2,3);
-        else
-            c = [0; 0];
-        end
+function x = project_div2(u, K, q, cc, Kp)
+    if isempty(cc) || nargin < 4
+        cc = [0 0];        
     end
+    
+    if nargin < 5
+        Kp = eye(3);
+    end
+    
+    C = eye(3);
+    C(1:2,3) = cc(1:2);    
 
-    if abs(q) > 0
-        m = size(u,1);
-        if (m == 2)
-            v = PT.homogenize(u);
-        else
-            v = u;
-        end
-        if nargin==4 && ~isempty(K)
-            v = K \ PT.renormI(v);
-        end
-        if ~isempty(K_new)
-            c = K_new \ [c'; 1];
-        end
+    x = C \ Kp \ u;
 
-        C = [1 0 c(1); 0 1 c(2); 0 0 1];
-        v = C \ v;
-
-        R = vecnorm(v(1:2,:),2,1);
-        Z = v(3,:);
+    keyboard;
+    if q ~= 0
+        R = vecnorm(x(1:2,:),2,1);
+        Z = x(3,:);
         r = max((Z+sqrt(Z.^2-4.*q.*R.^2))./(2.*q.*R),...
                        (Z-sqrt(Z.^2-4.*q.*R.^2))./(2.*q.*R));
-        v(1:2,:) = v(1:2,:) .* r ./ R;
-        v(3,:) = 1;
-        
-        v = C * v;
-
-        if ~isempty(K_new)
-            v = K_new * v;
-        end
-        if (m == 2)
-            v = v(1:2,:);
-        end
-    else
-        v = u;
+        x(1:2,:) = x(1:2,:) .* r ./ R;
+        x(3,:) = 1;
     end
-end
+    
+    x = K*C*x;

b88f3b2361b5af9454a06754db24ed13f8cd6a60
ylochman <lochman.y@gmail.com>
2020-08-06 17:25:09 -0400

    .

--- +PT/normalize.m
+++ +PT/normalize.m
@@ -4,6 +4,11 @@ function x = normalize(x, K, k)
     if nargin < 3
         k = 3;
     end
-    m = size(x, 1);
-    x = reshape(K \ reshape(x, k, []), m, []);
+    if iscell(x)
+        m = size(x{1}, 1);
+        x = cellfun(@(x0) reshape(K \ reshape(x0, k, []), m, []), x, 'UniformOutput', false);
+    else
+        m = size(x, 1);
+        x = reshape(K \ reshape(x, k, []), m, []);
+    end
 end
--- +PT/unnormalize.m
+++ +PT/unnormalize.m
@@ -2,6 +2,11 @@ function x = unnormalize(x, K, k)
     if nargin < 3
         k = 3;
     end
-    m = size(x, 1);
-    x = reshape(K * reshape(x, k, []), m, []);
+    if iscell(x)
+        m = size(x{1}, 1);
+        x = cellfun(@(x0) x = reshape(K * reshape(x0, k, []), m, []), x, 'UniformOutput', false);
+    else
+        m = size(x, 1);
+        x = reshape(K * reshape(x, k, []), m, []);
+    end
 end

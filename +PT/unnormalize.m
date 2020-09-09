d23605c94251e14732c88058638cd21bfec41884
ylochman <lochman.y@gmail.com>
2020-09-09 19:56:18 +0300

    .

--- +PT/unnormalize.m
+++ +PT/unnormalize.m
@@ -4,7 +4,7 @@ function x = unnormalize(x, K, k)
     end
     if iscell(x)
         m = size(x{1}, 1);
-        x = cellfun(@(x0) x = reshape(K * reshape(x0, k, []), m, []), x, 'UniformOutput', false);
+        x = cellfun(@(x0) reshape(K * reshape(x0, k, []), m, []), x, 'UniformOutput', false);
     else
         m = size(x, 1);
         x = reshape(K * reshape(x, k, []), m, []);

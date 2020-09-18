29824669fb0a15ddb965aba0ff66f32847cfccb4
ylochman <lochman.y@gmail.com>
2020-09-18 12:23:20 +0300

    .

--- +IMG/make_ld_masks.m
+++ +IMG/make_ld_masks.m
@@ -1,15 +1,15 @@
-function masks = make_ld_masks(ld, l, cntr, cntr_ud, nx, ny)
-    cntr_vl_dist = diag(l' * cntr_ud)';
-    cntr_vl_side = [cntr_vl_dist < 0];
+function masks = make_ld_masks(ld, l, pp, nx, ny)
+    pp_vl_dist = (l' * pp)';
+    pp_vl_side = [pp_vl_dist < 0];
 
-    clr.mask = {'w','k'};
+    clr.mask = {'k','w'};
     masks = {};
     for k=1:3
         masks01 = {};
         for s=0:1
             figure('visible','off');
-            rectangle('Position',[1 1 nx ny],'FaceColor',clr.mask{(cntr_vl_side(k)==s)+1});
-            rectangle('Position',[ld(1:2,k)'-ld(3,k) 2*ld(3,k) 2*ld(3,k)],'FaceColor',clr.mask{(cntr_vl_side(k)~=s)+1},'Curvature',[1 1]);
+            rectangle('Position',[1 1 nx ny],'FaceColor',clr.mask{(pp_vl_side(k)==s)+1});
+            rectangle('Position',[ld(1:2,k)'-ld(3,k) 2*ld(3,k) 2*ld(3,k)],'FaceColor',clr.mask{(pp_vl_side(k)~=s)+1},'Curvature',[1 1]);
             axis ij
             xlim([0,nx]);
             ylim([0,ny]);
--- +IMG/make_orthophotos.m
+++ +IMG/make_orthophotos.m
@@ -1,19 +1,20 @@
 function rimgs = make_orthophotos(img, masks, model, varargin)
     cfg.reiterate = false;
     cfg.plot = false;
-    cfg.vl_min_dist = 0.05;
+    cfg.vl_min_dist = 0.2;
     cfg.simgs = [];
     cfg.uimg = [];
     cfg.meas = [];
+    cfg.size = [];
     cfg = cmp_argparse(cfg, varargin{:});
+    if isempty(cfg.size)
+        cfg.size = 2*size(img);
+    end 
 
     nx = size(img,2);
     ny = size(img,1);
     
     vl_min_dist = cfg.vl_min_dist * max(ny,nx);
-    uimg = cfg.uimg;
-    simgs = cfg.simgs;
-    meas = cfg.meas;
 
     border_x = [1 nx nx 1; ny ny 1 1; 1 1 1 1];
     border_x_ud = CAM.backproject_div(border_x, model.K, model.proj_params);
@@ -22,45 +23,32 @@ function rimgs = make_orthophotos(img, masks, model, varargin)
 
     N = 3;
     if ~cfg.reiterate
-        rimgs = rectify(img, masks, model, vl_min_dist, border_x, border_vl_side);
+        rimgs = rectify(img, masks, model, vl_min_dist, border_x, border_vl_side, cfg);
     else
         while true
-            rimgs = rectify(img, masks, model, vl_min_dist, border_x, border_vl_side);
-            if cfg.plot
-                fig;
-                subplot(4,2+N,1); imshow(img); MEAS.draw(meas)
-                subplot(4,2+N,2); imshow(uimg);
-                for s = 0:1
-                    for k = 1:N
-                        subplot(4,2+N,(2+N)*(2*s)+k+2)
-                        imshow(simgs{k}{s+1});
-                        if ~isnan(rimgs{k}{s+1})
-                            subplot(4,2+N,(2+N)*(2*s+1)+k+2)
-                            imshow(rimgs{k}{s+1});
-                        end
-                    end
-                end
-                ans = input('Retry(Y/y/N/n): ', 's');
-                if strcmp(upper(ans),'N')
-                    break
-                else
-                    display(['Current vl_min_dist is: ' num2str(vl_min_dist)])
-                    ans = input('Input new one or skip (ENTER): ');
-                    if ~isempty(ans)
-                        vl_min_dist = ans;
-                    else
-                        break
-                    end
-                end
-            else
+            rimgs = rectify(img, masks, model, vl_min_dist, border_x, border_vl_side, cfg);
+            ans = input('Retry(Y/y/N/n): ', 's');
+            if strcmp(upper(ans),'N')
                 break
+            else
+                display(['Current vl_min_dist is: ' num2str(vl_min_dist)])
+                ans = input('Input new one or skip (ENTER): ');
+                if ~isempty(ans)
+                    vl_min_dist = ans;
+                else
+                    break
+                end
             end
         end
     end
 end
 
-function rimgs = rectify(img, masks, model, vl_min_dist, border_x, border_vl_side)
+function rimgs = rectify(img, masks, model, vl_min_dist, border_x, border_vl_side, cfg)
     N = 3;
+
+    uimg = cfg.uimg;
+    simgs = cfg.simgs;
+    meas = cfg.meas;
     
     nx = size(img,2);
     ny = size(img,1);
@@ -88,11 +76,27 @@ function rimgs = rectify(img, masks, model, vl_min_dist, border_x, border_vl_sid
                                     max(img,masks{k}{s+1}),...
                                     model.Hs(:,:,k),...
                                     model.cc, model.q, ...
-                                    'size', size(img),...
+                                    'size', cfg.size,...
                                     'border', border(1:2,:)', ...
                                     'Registration', 'none');
             else
                 rimgs{k}{s+1} = NaN;
+            end
+        end
+    end
+
+    if cfg.plot
+        fig;
+        subplot(4,2+N,1); imshow(img); MEAS.draw(meas)
+        subplot(4,2+N,2); imshow(uimg);
+        for s = 0:1
+            for k = 1:N
+                subplot(4,2+N,(2+N)*(2*s)+k+2)
+                imshow(simgs{k}{s+1});
+                if ~isnan(rimgs{k}{s+1})
+                    subplot(4,2+N,(2+N)*(2*s+1)+k+2)
+                    imshow(rimgs{k}{s+1});
+                end
             end
         end
     end
